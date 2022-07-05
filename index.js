442dde604c7143bb8874e0c6db3ece8fcad3383f
Dhruv <106154136+dhruv-githb@users.noreply.github.com>
2022-07-05 13:48:11 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const videoCards = [...document.querySelectorAll('.video-card')];
+videoCards.forEach(item => {
+    item.addEventListener('mouseover', () => {
+        let video = item.children[1];
+        video.play();
+    })
+    item.addEventListener('mouseleave', () => {
+        let video = item.children[1];
+        video.pause();
+    })
+})
+
+let cardContainers = [...document.querySelectorAll('.card-container')];
+let preBtns = [...document.querySelectorAll('.pre-btn')];
+let nextBtns = [...document.querySelectorAll('.nxt-btn')];
+
+cardContainers.forEach((item,i)=>{
+    let containerDimensions= item.getBoundingClientRect();
+    let containerWidth= containerDimensions.width;
+
+    nextBtns[i].addEventListener('click', () => {
+        item.scrollLeft +=containerWidth -200;
+    })
+    preBtns[i].addEventListener('click', () => {
+        item.scrollLeft -=containerWidth +200;
+    })
+})
